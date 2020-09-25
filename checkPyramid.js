fd3326dc4752aee1452ab3e3842d87ac46751561
prajeet21 <prajeetbhavsar97@gmail.com>
2020-09-24 20:57:13 -0700

    Code refactoring

--- checkPyramid.js
+++ checkPyramid.js
@@ -1,7 +1,10 @@
+// Function to check wheter the word is a pyramid word or not
+
 function checkPyramid(str) {
-  str = str.toLowerCase();
-  console.log("Input is: " + str);
-  let frequency = {};
+  str = str.toLowerCase(); // convert the input to lower case
+  let frequency = {}; // object to store frequencies of each characters {char: frequency}
+
+  // Build the frequency object
   for (let i = 0; i < str.length; i++) {
     let ch = str.charAt(i);
     if (frequency[ch]) {
@@ -11,22 +14,28 @@ function checkPyramid(str) {
     }
   }
 
-  console.log("Frequency object: " + frequency);
+  // console.log("Frequency object: " + frequency);
 
+  // Sort the frequencies by their values
   let sortedValues = Object.values(frequency).sort();
-  console.log(sortedValues);
+  // console.log(sortedValues);
 
   let result = true;
 
+  // first value must be 1
+  if (sortedValues[0] !== 1) {
+    return false;
+  }
+
+  // check if the frequency values are increasing by 1
   for (let i = 0; i < sortedValues.length - 1; i++) {
-    console.log("Comparing " + sortedValues[i] + " and " + sortedValues[i + 1]);
     if (sortedValues[i + 1] !== sortedValues[i] + 1) {
       result = false;
       break;
     }
   }
 
-  console.log("Boolean result: " + result);
+  return result;
 }
 
 module.exports = checkPyramid;
--- index.js
+++ index.js
@@ -5,23 +5,28 @@ const checkPyramid = require(__dirname + "/checkPyramid.js");
 
 const app = express();
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let message = "This is the message";
+let message = "";
 
 app.get("/", (req, res) => {
-    res.render("index", {message: message});
+  res.render("index", { message: message });
 });
 
 app.post("/", (req, res) => {
-    const inputWord = req.body.inputWord;
-    // add logic here
-    const result = checkPyramid(inputWord);
-    console.log("This is the result" + result);
-    res.redirect("/");
+  const inputWord = req.body.inputWord;
+  // add logic here
+  const result = checkPyramid(inputWord);
+  console.log("This is the result: " + result);
+  if (result) {
+    message = inputWord + " is a pyramid word";
+  } else {
+    message = inputWord + " is not a pyramid word";
+  }
+  res.redirect("/");
 });
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000...");
+  console.log("Server is running on port 3000...");
 });
