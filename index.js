3fc2df0cbc348f49ce8a4f807e35ca7c999b31bb
prajeet21 <prajeetbhavsar97@gmail.com>
2020-09-24 17:40:07 -0700

    Use EJS

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html");
+    res.sendFile("index");
+});
+
+app.post("/", (req, res) => {
+    const inputWord = req.body.inputWord;
+    console.log(inputWord);
 });
 
 app.listen(3000, () => {
